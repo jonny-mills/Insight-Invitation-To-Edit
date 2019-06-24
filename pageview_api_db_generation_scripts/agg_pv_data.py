098fc04ac7f5d94703b7e731996c8641da3cf6a6
jonny-mills <35629096+jonny-mills@users.noreply.github.com>
2019-06-23 17:02:15 -0700

    Update agg_pv_data.py

--- pageview_api_db_generation_scripts/agg_pv_data.py
+++ pageview_api_db_generation_scripts/agg_pv_data.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 20 09:24:58 2019
-
-@author: isabel
+File description:
+Input: a file containing pageviews for every single Wikipedia of every day for an entire month
+Processing: dataframe aggregate pageview statistics for each month are computed for each list page. Then dataframe is transposed
+Output: A transposed datafrom with the rows being every single list page on Wiki, and each column is a different month of the year.
 """
 
 import pandas as pd
